1079de43a4563562e65175d0ad5e3e61b5a85d4d
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-09 00:11:14 -0300

    feat/ add css to page product

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { PRODUCTS } from "../../products";
 import { Product } from "./product";
+import "./shop.css";
 
 export const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-        <h1>Produtos Eletrônicos</h1>
+        <h1>Shopping Floripa</h1>
       </div>
       <div className="products">
         {PRODUCTS.map((product) => (
