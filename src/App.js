952cb9d8954ba6f584409e4c2d7503e7f59674cc
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-08 23:16:10 -0300

    feat/create pages products checkou and 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
+import { Products } from "./pages/products/products";
+import { Checkout } from "./pages/checkout/checkout";
+import { Problem } from "./pages/404/404";
 
 function App() {
   return (
@@ -8,11 +13,11 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" />
-          <Route path="/products" />
-          <Route path="/cart" />
-          <Route path="/checkout" />
-          <Route path="/404" />
+          <Route path="/" element={<Shop />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/404" element={<Problem />} />
         </Routes>
       </Router>
     </div>
