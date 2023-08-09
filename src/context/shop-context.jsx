f0c0be1b9fbc77adba84d9e65478557bccf4d6a1
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-09 00:24:32 -0300

    feat/ create context

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,7 @@
+import React, { createContext } from "react";
+
+export const ShopContext = createContext(null);
+
+export const ShopContextProvider = (props) => {
+  return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
+};
