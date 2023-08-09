754e404e37a97d6b6aa0b598c68a966cb9d42335
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-09 00:43:50 -0300

    feat/ Add Shop Context Provider to App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import { Shop } from "./pages/shop/shop";
 import { Cart } from "./pages/cart/cart";
 import { Checkout } from "./pages/checkout/checkout";
 import { Problem } from "./pages/404/404";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/404" element={<Problem />} />
-        </Routes>
-      </Router>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/404" element={<Problem />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
